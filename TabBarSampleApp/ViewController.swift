01bdcbb16672d73618bb787b73c411f33e1b1360
Hamid <mail.hamid.hoseini@gmail.com>
2020-09-30 01:49:56 -0700

    first commit

--- TabBarSampleApp/ViewController.swift
+++ TabBarSampleApp/ViewController.swift
@@ -9,11 +9,102 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let button: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 52))
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = .white
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBlue
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.center = view.center
+    }
+    
+    @objc func didTapButton() {
+        // Create and present tap bar controller
+        let tabBarVC = UITabBarController()
+        let vc1 = UINavigationController(rootViewController: firstViewController())
+        let vc2 = UINavigationController(rootViewController: secondViewController())
+        let vc3 = UINavigationController(rootViewController: thirdViewController())
+        let vc4 = UINavigationController(rootViewController: fourthViewController())
+        let vc5 = UINavigationController(rootViewController: fifthViewController())
+        
+        vc1.title = "Home"
+        vc2.title = "Contact"
+        vc3.title = "Help"
+        vc4.title = "About"
+        vc5.title = "Setting"
+        tabBarVC.setViewControllers([vc1, vc2, vc3, vc4, vc5], animated: false)
+        
+        guard let items = tabBarVC.tabBar.items else {
+            return
+        }
+        
+        let images = ["house", "bell", "person.circle", "star", "gear"]
+        
+        for x in 0..<items.count {
+            items[x].image = UIImage(systemName: images[x])
+        }
+        
+//        for item in items {
+//            item.image = UIImage(systemName: "house")
+//        }
+        
+        tabBarVC.modalPresentationStyle = .fullScreen
+        present(tabBarVC, animated: true, completion: nil)
     }
 
 
+}
+
+class firstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        title = "Home"
+    }
+}
+
+class secondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        title = "Contact"
+    }
+}
+
+class thirdViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+        title = "Help"
+    }
+}
+
+class fourthViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        title = "About"
+    }
+}
+
+class fifthViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+        title = "Setting"
+    }
 }
 
